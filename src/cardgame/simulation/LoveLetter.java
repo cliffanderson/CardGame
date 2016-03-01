f300d97329d71959ce7eb95ebab0ee949d51854d
Thomas Plano <planot@wit.edu>
2016-03-01 18:01:30 -0500

    s

--- src/cardgame/simulation/LoveLetter.java
+++ src/cardgame/simulation/LoveLetter.java
@@ -4,7 +4,7 @@ package cardgame.simulation;
  * Created by planot on 3/1/2016.
  */
 public class LoveLetter extends Engine {
-    public LoveLetter(){
+    public LoveLetter(int NumofPlayers){
         //shuffle deck
         //deal one card to each player
         //burn a card
