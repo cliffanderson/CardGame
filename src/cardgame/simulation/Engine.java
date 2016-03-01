f8ae6f3848d282740004110058f250f5dfeb0c89
Thomas Plano <planot@wit.edu>
2016-03-01 09:55:42 -0500

    started the game engine

---
+++ src/cardgame/simulation/Engine.java
@@ -0,0 +1,24 @@
+package cardgame.simulation;
+
+/**
+ * Created by planot on 3/1/2016.
+ */
+public abstract class Engine {
+    public Engine(){}
+
+    public void draw(){
+        //call deck class
+    }
+
+    public void shuffle(){
+       //call deck class
+    }
+    public void play_card(){
+        //put card from hand into play space
+    }
+
+}
+
+
+
+
---
+++ src/cardgame/simulation/GoFish.java
@@ -0,0 +1,8 @@
+package cardgame.simulation;
+
+/**
+ * Created by planot on 3/1/2016.
+ */
+public class GoFish extends Engine {
+    public void GoFish(){}
+}
