18d472cb28c7876fc8c875221a268dc4966de046
Thomas Plano <planot@wit.edu>
2016-03-02 09:08:16 -0500

    ugh

--- src/cardgame/simulation/Card.java
+++ src/cardgame/simulation/Card.java
@@ -4,4 +4,7 @@ package cardgame.simulation;
  * Created by andersonc12 on 3/1/2016.
  */
 public class Card {
+    public Card(){
+
+    }
 }
--- src/cardgame/simulation/Engine.java
+++ src/cardgame/simulation/Engine.java
@@ -4,7 +4,6 @@ package cardgame.simulation;
  * Created by planot on 3/1/2016.
  */
 public abstract class Engine {
-    public Engine(){}
 
     public void draw(){
         //call deck class
@@ -16,7 +15,9 @@ public abstract class Engine {
     public void play_card(){
         //put card from hand into play space
     }
-
+    Deck LoadedDeck;
+    Player[] Players;
+    int NumofPlayers;
 }
 
 
