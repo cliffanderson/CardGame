bdf809ae6018a8b435a81a99d10d0b596a3ad0ba
Artekis <Artekis@users.noreply.github.com>
2016-03-03 09:04:37 -0500

    Setting up MultiClient-Server

--- src/server_client/MultiClient_Server.java
+++ src/server_client/MultiClient_Server.java
@@ -1,4 +1,23 @@
 package server_client;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class MultiClient_Server {
+
+    private static int connectionNum = 0;
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(50000);
+        Socket clientSocket = null;
+        Socket[] sockets = new Socket[10];
+
+        while (true) {
+            clientSocket = serverSocket.accept();
+
+            new ClientConnection();
+        }
+    }
 }
