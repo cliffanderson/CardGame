d3c4de980e3d2bc950a1d1be7ad4cbfabb1236ce
Artur Janowiec <artekisx@gmail.com>
2016-03-06 18:27:06 -0500

    Implemented Feature #2 - Need to fix LinkedList Counter

--- src/server_client/MultiClient_Server.java
+++ src/server_client/MultiClient_Server.java
@@ -3,6 +3,7 @@ package server_client;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.LinkedList;
 
 /** Class that has the capabilities to act as a server, constantly accept multiple connections requests from clients, broadcast to those clients, and
  * listen to input from those clients.
@@ -12,14 +13,14 @@ import java.net.Socket;
 public class MultiClient_Server {
 
     private ServerSocket serverSocket;
-    private Socket[] sockets;
-    private DataOutputStream[] toClientStreams;
-    private DataInputStream[] fromClientStreams;
+    private LinkedList<Socket> sockets;
+    private LinkedList<DataOutputStream> toClientStreams;
+    private LinkedList<DataInputStream> fromClientStreams;
 
     private ServerStatus status;
     private int numberConnections;
 
-    /** Setups the server with a default port number of 50000, and a max number of connections equal to 10.
+    /** Setups the server with a default port number of 50000.
      * */
     public MultiClient_Server() throws IOException{
         status = ServerStatus.RUNNING;
@@ -27,31 +28,30 @@ public class MultiClient_Server {
 
         serverSocket = new ServerSocket(50000);
 
-        sockets = new Socket[10];
-        toClientStreams = new DataOutputStream[10];
-        fromClientStreams = new DataInputStream[10];
+        sockets = new LinkedList<>();
+        toClientStreams = new LinkedList<>();
+        fromClientStreams = new LinkedList<>();
     }
 
-    /** Setups the server with a specified port number and a max number of connections possible.
+    /** Setups the server with a specified port number.
      * @param portNumber the port number that the server runs on.
-     * @param maxConnections the max number of connections possible for this server.
      * */
-    public MultiClient_Server(int portNumber, int maxConnections) throws IOException{
+    public MultiClient_Server(int portNumber) throws IOException{
         status = ServerStatus.RUNNING;
         numberConnections = 0;
 
         serverSocket = new ServerSocket(portNumber);
 
-        sockets = new Socket[maxConnections];
-        toClientStreams = new DataOutputStream[maxConnections];
-        fromClientStreams = new DataInputStream[maxConnections];
+        sockets = new LinkedList<>();
+        toClientStreams = new LinkedList<>();
+        fromClientStreams = new LinkedList<>();
     }
 
     private enum ServerStatus {RUNNING, SHUTTING_DOWN};
 
     public static void main(String[] args) throws IOException {
 
-        MultiClient_Server server = new MultiClient_Server(50000, 10);
+        MultiClient_Server server = new MultiClient_Server(50000);
 
         server.acceptConnections();
         server.handleClients();
@@ -68,9 +68,9 @@ public class MultiClient_Server {
                         Socket clientSocket = null;
                         clientSocket = serverSocket.accept();
 
-                        sockets[numberConnections] = clientSocket;
-                        toClientStreams[numberConnections] = new DataOutputStream(clientSocket.getOutputStream());
-                        fromClientStreams[numberConnections] = new DataInputStream(clientSocket.getInputStream());
+                        sockets.add(clientSocket);
+                        toClientStreams.add(new DataOutputStream(clientSocket.getOutputStream()));
+                        fromClientStreams.add(new DataInputStream(clientSocket.getInputStream()));
 
                         numberConnections++;
                     } catch (IOException e) {
@@ -82,6 +82,7 @@ public class MultiClient_Server {
     }
 
     /** Constantly listens to clients, and broadcasts any messages from the client to all the clients connected to the server.
+     * Each client receives their own thread on the server.
      * */
     public void handleClients() {
 
@@ -107,6 +108,8 @@ public class MultiClient_Server {
         }.start();
     }
 
+    /** Inner class thread that is used by handleClients() to handle reading messages from clients and broadcast those messages to all clients.
+     * */
     public class HandleClientThread implements Runnable {
 
         private int clientNumber;
@@ -119,7 +122,7 @@ public class MultiClient_Server {
         public void run() {
             try {
                 while (status == ServerStatus.RUNNING) {
-                    String message = fromClientStreams[clientNumber].readUTF();
+                    String message = fromClientStreams.get(clientNumber).readUTF();
                     broadcastClientMessage(message);
                     //System.out.println(message);
                 }
@@ -136,13 +139,32 @@ public class MultiClient_Server {
 
     /** Broadcasts a client message once to every client connected to the server.
      * */
-    private void broadcastClientMessage(String message) {
+    private void broadcastClientMessage(String message) throws IOException {
         for (int i = 0; i < numberConnections; i++) {
             try {
-                toClientStreams[i].writeUTF(message);
+                toClientStreams.get(i).writeUTF(message);
             } catch (IOException e) {
                 System.out.println("Exception: " + e.getMessage() + " in broadClientMessage().");
+                closeClientConnection(i);
             }
+        }
+    }
+
+    /** Closes a client connection.
+     * @param clientNumber the client connection number to be closed.
+     * */
+    public void closeClientConnection(int clientNumber) throws IOException {
+        try {
+            toClientStreams.get(clientNumber).close();
+            fromClientStreams.get(clientNumber).close();
+            sockets.get(clientNumber).close();
+            toClientStreams.remove(clientNumber);
+            fromClientStreams.remove(clientNumber);
+            sockets.get(clientNumber);
+
+            System.out.println("Client connection" + clientNumber + " successfully closed");
+        } catch (IOException e) {
+            System.out.println("Exception: " + e.getMessage() + "in closeClientConnection");
         }
     }
 
@@ -152,7 +174,7 @@ public class MultiClient_Server {
     public void broadcastServerMessage(String message) {
         try {
             for (int i = 0; i < numberConnections; i++) {
-                toClientStreams[i].writeUTF(message);
+                toClientStreams.get(i).writeUTF(message);
             }
             Thread.sleep(10000);
         } catch (IOException e) {
@@ -168,7 +190,7 @@ public class MultiClient_Server {
      * */
     public void sendToClient(String message, int clientNumber) {
         try {
-            toClientStreams[clientNumber].writeUTF(message);
+            toClientStreams.get(clientNumber).writeUTF(message);
         } catch (IOException e) {
             System.out.println("Exception: " + e.getMessage() + " in broadcastToClient().");
         }
